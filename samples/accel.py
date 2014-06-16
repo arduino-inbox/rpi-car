62336cb1ae4ddc2188ad270cb94941aa2bfb25dd
Roman Stets <romko.stets@gmail.com>
2014-06-16 14:08:39 +0300

    Cleanup.

--- samples/accel.py
+++ samples/accel.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=utf-8
 
 import smbus
 import math
@@ -7,41 +8,47 @@ import math
 power_mgmt_1 = 0x6b
 power_mgmt_2 = 0x6c
 
+
 def read_byte(adr):
     return bus.read_byte_data(address, adr)
 
+
 def read_word(adr):
     high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr+1)
+    low = bus.read_byte_data(address, adr + 1)
     val = (high << 8) + low
     return val
 
+
 def read_word_2c(adr):
     val = read_word(adr)
-    if (val >= 0x8000):
+    if val >= 0x8000:
         return -((65535 - val) + 1)
     else:
         return val
 
-def dist(a,b):
-    return math.sqrt((a*a)+(b*b))
 
-def get_y_rotation(x,y,z):
-    radians = math.atan2(x, dist(y,z))
+def dist(a, b):
+    return math.sqrt((a * a) + (b * b))
+
+
+def get_y_rotation(x, y, z):
+    radians = math.atan2(x, dist(y, z))
     return -math.degrees(radians)
 
-def get_x_rotation(x,y,z):
-    radians = math.atan2(y, dist(x,z))
+
+def get_x_rotation(x, y, z):
+    radians = math.atan2(y, dist(x, z))
     return math.degrees(radians)
 
-bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
-address = 0x68       # This is the address value read via the i2cdetect command
+
+bus = smbus.SMBus(1)  # or bus = smbus.SMBus(1) for Revision 2 boards
+address = 0x68        # This is the address value read via the i2cdetect command
 
 # Now wake the 6050 up as it starts in sleep mode
 bus.write_byte_data(address, power_mgmt_1, 0)
 
 while True:
-
     gyro = (read_word_2c(0x43), read_word_2c(0x45), read_word_2c(0x47),)
     accel = (read_word_2c(0x3b), read_word_2c(0x3d), read_word_2c(0x3f),)
 
