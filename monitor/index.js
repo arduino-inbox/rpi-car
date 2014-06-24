5f72c0984fb7bb0ff4af23eb38a2d3881e98b609
Roman Stets <romko.stets@gmail.com>
2014-06-24 12:59:20 +0300

    wip

--- monitor/index.js
+++ monitor/index.js
@@ -1,108 +1,58 @@
-var clients = [];
-var data_key;
+var c = null;
 
-init = function(httpd, key) {
-	var io = require('socket.io').listen(httpd);
-
-	data_key = key;
-
-	io.sockets.on('connection', function(socket) {
-		clients.push(socket);
-		console.log( "Client connected: " + socket );
-	});
-
-	io.sockets.on('disconnect', function(socket) {
-		clients.pull( clients.indexOf(socket) );
-		console.log( "Client disconnected: " + socket );
-	});
-
-};
-
-async = require('async');
-emit = function(data) {
-	if ( clients.length > 0 ) {
-		async.eachSeries(
-			clients,
-			function(socket, callback) {
-				socket.emit(data_key, data);
-				callback();
-			},
-			function(err) {
-				//console.info(err);
-			}
-		);
-	}
-};
-
-var static = require('node-static');
-var webfolder = new static.Server('.');
-
-processData = function(document) {
-	emit(document);
-};
+var s = require('node-static');
+var webfolder = new s.Server('.');
 
 // init HTTP demon
-var httpd = require('http').createServer(function(req,res) {
-	req.addListener('end', function() {
-		webfolder.serve(req,res);
-	}
-	).resume();
-}
-);
+var httpd = require('http').createServer(function (req, res) {
+    req.addListener('end', function () {
+        webfolder.serve(req, res);
+    }).resume();
+});
 httpd.listen(8080);
 
 // init client dispatcher
-init(httpd, 'measurements_update');
+var io = require('socket.io').listen(httpd);
+
+io.sockets.on('connection', function (socket) {
+    c = socket;
+    console.log("Client connected");
+});
+
+io.sockets.on('disconnect', function (socket) {
+    c = null;
+    console.log("Client disconnected");
+});
 
 var redis = require("redis"),
-        client = redis.createClient(null, 'alarmpi.local');
+    client = redis.createClient(null, 'alarmpi.local');
 
 client.on("error", function (err) {
-        console.log("Err:", err);
-    });
+    console.log("Err:", err);
+});
 
 var dataPoints = [
-    'test-gyro-0',
-    'test-gyro-1',
-    'test-gyro-2',
+//    'test-gyro-0',
+//    'test-gyro-1',
+//    'test-gyro-2',
     'test-accel-0',
-    'test-accel-1',
-    'test-accel-2'
+    'test-accel-1'
+//    'test-accel-2'
 ];
 
 loop = function () {
-//  client.get('acceleration', function (err, data) {
-//    if (data) {
-//      try {
-//        var value = parseFloat(data);
-//        processData(value);
-//        //console.log("Value:", value);
-//      } catch (e) {
-//        //console.log("Error ", e);
-//      }
-//    }
-//    process.nextTick(loop);
-//  });
-
-  dataPoints.forEach(function (p) {
-      client.get(p, function (err, data) {
-        if (err) {
-            console.log("Err:", err);
-        }
-        else if (data) {
-          try {
-            processData({
-                key: p,
-                value: data
-            });
-            //console.log("Value:", data);
-          } catch (e) {
-            console.log("Err:", e);
-          }
-        }
-        process.nextTick(loop);
-      });
-  });
+    dataPoints.forEach(function (p) {
+        client.get(p, function (err, data) {
+            if (data) {
+                c.emit('measurements_update', {
+                    key: p,
+                    value: data
+                });
+                //console.log("Value:", data);
+            }
+            process.nextTick(loop);
+        });
+    });
 };
 
 process.nextTick(loop);
