3cd0fe3c822b04987c115bb1436f30ad9f0d9a5a
Roman Stets <rstets@rebbix.com>
2015-01-21 18:01:26 +0200

    wip

--- car.js/robot.js
+++ car.js/robot.js
@@ -1,5 +1,5 @@
 var ultrasonic = new (require('./nodes/ultrasonic'))();
-setTimeout(function () {
+//setTimeout(function () {
   var distance = ultrasonic.getDistance();
   console.log('distance:', distance);
-}, 30); // warm up timeout
+//}, 30); // warm up timeout
