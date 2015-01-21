c4299db926a07ec26380eeade5b1d6a6ac695bcf
Roman Stets <rstets@rebbix.com>
2015-01-21 17:22:04 +0200

    wip

--- car.js/nodes/ultrasonic.js
+++ car.js/nodes/ultrasonic.js
@@ -1,12 +1,12 @@
 module.exports = function (config) {
   var usonic = require('r-pi-usonic');
   var self = this;
-
-  self.echoPin = config.echoPin || 22;
-  self.triggerPin = config.triggerPin || 24;
-  self.timeout = config.timeout || 200;
-  self.delay = config.delay || 30;
-  self.sensor = usonic.sensor(self.echoPin, self.triggerPin, self.timeout);
+  self.config = config || {
+    echoPin: 22,
+    triggerPin: 24,
+    timeout: 200
+  };
+  self.sensor = usonic.sensor(self.config.echoPin, self.config.triggerPin, self.config.timeout);
 
   return {
     getDistance: function () {
