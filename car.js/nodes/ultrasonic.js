837837d38c7b826b0d9e092e91908631b2564cd7
Roman Stets <rstets@rebbix.com>
2015-01-21 17:24:05 +0200

    wip

--- car.js/nodes/ultrasonic.js
+++ car.js/nodes/ultrasonic.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
   self.config = config || {
     echoPin: 22,
     triggerPin: 24,
-    timeout: 1000
+    timeout: 100
   };
   self.sensor = usonic.sensor(self.config.echoPin, self.config.triggerPin, self.config.timeout);
 
