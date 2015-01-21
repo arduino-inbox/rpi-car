c66d074641272e612c74e04f72fa32ba6760bfb6
Roman Stets <rstets@rebbix.com>
2015-01-21 18:33:40 +0200

    wip

--- car.js/nodes/ultrasonic.js
+++ car.js/nodes/ultrasonic.js
@@ -1,16 +1,22 @@
 module.exports = function (config) {
   var usonic = require('r-pi-usonic');
+  var events = require('events');
   var self = this;
-  self.config = config || {
-    echoPin: 22,
-    triggerPin: 24,
-    timeout: 500 // smaller values always return -1
-  };
+
+  self.config = config;
   self.sensor = usonic.sensor(self.config.echoPin, self.config.triggerPin, self.config.timeout);
+  self.distance = 2^16 - 1;
+
+  events.EventEmitter.call(self);
+
+  var update = function () {
+    self.distance = self.sensor();
+    self.emit('update', 'distance', self.distance);
+  };
 
   return {
-    getDistance: function () {
-      return self.sensor();
+    work: function () {
+      setInterval(update, self.config.interval);
     }
   }
 };
--- car.js/robot.js
+++ car.js/robot.js
@@ -1,5 +1,46 @@
-var ultrasonic = new (require('./nodes/ultrasonic'))();
-//setTimeout(function () {
-  var distance = ultrasonic.getDistance();
-  console.log('distance:', distance);
-//}, 30); // warm up timeout
+var Robot = function (config) {
+  var self = this;
+  self.config = config;
+  self.nodes = [];
+
+  console.log("Configuring nodes");
+  self.config.nodes.forEach(function (node) {
+    console.log("-", node.name);
+    var nodeClass = require('./nodes/' + node.name);
+    var nodeInstance = new nodeClass(node.config);
+    self.nodes.push({
+      name: node.name,
+      instance: nodeInstance
+    });
+  });
+
+  return {
+    work: function () {
+      console.log("Starting nodes");
+      self.nodes.forEach(function (node) {
+        console.log("-", node.name);
+        node.instance.on('update', function (param, value) {
+          console.log("Node update:", node.name);
+          console.log(param, "=", value);
+        });
+        node.instance.work();
+      });
+    }
+  }
+};
+
+var robot = new Robot({
+  nodes: [
+    {
+      name: "ultrasonic",
+      config: {
+        echoPin: 22,
+        triggerPin: 24,
+        timeout: 500, // smaller values always return -1
+        interval: 1000 // @todo to fiddle with
+      }
+    }
+  ]
+});
+
+robot.work();
