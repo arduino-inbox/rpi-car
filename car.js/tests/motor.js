f1784a35b3529b3e15ca717ecab9e619fa7f1766
Roman Stets <rstets@rebbix.com>
2015-01-22 11:15:36 +0200

    motor test

--- car.js/tests/motor.js
+++ car.js/tests/motor.js
@@ -14,18 +14,22 @@ d2.writeSync(0);
 increaseSpeed = function () {
   piblaster.setPwm(pwmPin, speed);
   speed += 0.1;
+  if (speed > 1) speed = 1; // fix
+
   if (speed < 1) {
-    setTimeout(increaseSpeed, 10);
+    setTimeout(increaseSpeed, 300);
   } else {
-    setTimeout(decreaseSpeed, 10);
+    setTimeout(decreaseSpeed, 300);
   }
 };
 
 decreaseSpeed = function () {
   piblaster.setPwm(pwmPin, speed);
   speed -= 0.1;
+  if (speed < 0) speed = 0; // fix
+
   if (speed > 0) {
-    setTimeout(decreaseSpeed, 10);
+    setTimeout(decreaseSpeed, 300);
   }
 };
 
