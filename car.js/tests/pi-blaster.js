ff7c2c520bcb2a7d3a25ddd45247681e7c3a0ab6
Roman Stets <romko.stets@gmail.com>
2015-02-07 19:07:35 +0200

    wip

--- car.js/tests/pi-blaster.js
+++ car.js/tests/pi-blaster.js
@@ -2,15 +2,13 @@ var events = require('events');
 var child = require('child_process');
 
 console.log("child.spawn('pi-blaster')");
-piBlasterProc = child.spawn('pi-blaster');
-
-piBlasterProc.stdout.on('data', function (data) {
-    console.log("pi-blaster::stdout: " + data);
-});
-
-piBlasterProc.stderr.on('data', function (data) {
-    console.log("pi-blaster::stderr: " + data);
-});
+piBlasterProc = child.spawn(
+    'pi-blaster',
+    {
+        stdio: [0, 'pipe', 'pipe']
+    }
+);
+console.log('Spawned child pid: ' + piBlasterProc.pid);
 
 piBlasterProc.on('close', function (code, signal) {
     console.log("error", "pi-blaster exited with code " + code + " on " + signal + " signal.");
