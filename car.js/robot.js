05283bdbc1fde97abb0d02c3adc1c502235487b3
Roman Stets <romko.stets@gmail.com>
2015-02-07 14:55:55 +0200

    wip

--- car.js/robot.js
+++ car.js/robot.js
@@ -35,8 +35,7 @@ function Robot(config) {
       transports: [
         new (winston.transports.Console)({
           colorize: 'all',
-          level: 'debug',
-          handleExceptions: true
+          level: 'debug'
         })
       ]
     });
