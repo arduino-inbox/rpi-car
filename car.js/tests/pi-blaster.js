dedc177eb61ff3b50d8fbca867925c9d2171cee4
Roman Stets <romko.stets@gmail.com>
2015-02-07 19:35:46 +0200

    wip

--- car.js/tests/pi-blaster.js
+++ car.js/tests/pi-blaster.js
@@ -1,17 +1,10 @@
 var events = require('events');
 var spawn = require('child_process').spawn;
-var pb = spawn('pi-blaster');
-pb.stdout.on('data', function (data) {
-  console.log('stdout: ' + data);
-});
-
-pb.stderr.on('data', function (data) {
-  console.log('stderr: ' + data);
-});
-
-pb.on('exit', function (code) {
+var pb = spawn('pi-blaster', function (code, stdout, stderr) {
   console.log('child process exited with code ' + code);
+  console.log(stdout, stderr);
 });
+console.log("pid:", pb.pid);
 
 //exec('killall pi-blaster', function () {
 //  var piBlaster = exec('pi-blaster', function (error) {
